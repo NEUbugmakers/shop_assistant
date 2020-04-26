1dfb6672d11a54581bc889737f470bbd510e65f1
Ainz <59441476+461720777@users.noreply.github.com>
2020-04-26 20:47:13 +0800

    Update and rename goods.h to C_goods.h

---
+++ code/Goods_shelf_inventory_management/C_goods.h
@@ -0,0 +1,17 @@
+#pragma once
+#include"Date.h"
+#define C_NUM_LEN    19//编码长度
+#define C_NAME_LEN   100//名称最大长度
+typedef struct{//商品信息
+	int C_in;//进价
+	int C_out;//标价（非实售价）
+	char name[C_NAME_LEN];//商品名
+	char num[C_NUM_LEN];//编码
+	Date C_shelfDate;//保质期
+	Date C_producedDate;//生产日期
+	Date C_entryDate;//入库日期
+	Date C_rotDate;//过期日期
+} C_Goods ;
+int C_Goods_LessThan(C_Goods AGoods, C_Goods BGoods) {//比较较商品过期日期的大小
+
+}
