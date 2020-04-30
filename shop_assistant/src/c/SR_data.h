47004aa2e66598e2f35703f03d09a7955d2df875
佳伟 汤 <461720777@qq.com>
2020-04-30 21:41:37 +0800

    完善B_getSrc 未完成的SR_data

--- shop_assistant/src/c/SR_data.h
+++ shop_assistant/src/c/SR_data.h
@@ -12,13 +12,40 @@ typedef struct SR_dataBTNodeT  SR_dataBTNode;
 struct SR_dataBTNodeT {//B-树节点
 	SR_dataBTNode* parent;
 	char name[SR_BTNODE_NAME_LEN];
-	B_list* child;
-	B_list* childCode;
-	FILE* file;
+	char sort;
+	int childNum;//子节点的数量
+	B_vector* child;
+	B_vector* childCode;
+	char fileName[10];
+	B_vector* goodsVector;
 };
 SR_dataBTNode SR_dataBTRoot;
-void SR_dataBTOpen() {
-	FILE* file = fopen("root", "ab+");
-	SR_dataBTNode* x = (SR_dataBTNode*)malloc(sizeof(SR_dataBTNode));
-	B_getSrc(file, x,sizeof(SR_dataBTNode));
+void SR_dataBTInit() {//初始化B-树
+	FILE* file = fopen("root", "rb+");
+	B_getSrc(file, &SR_dataBTRoot, sizeof(SR_dataBTNode));
+	SR_dataBTBuild(&SR_dataBTRoot, file);
+}
+void SR_dataBTBuild(SR_dataBTNode* x, FILE* file) {//根据文件建立B-树
+	if (x->childNum > 0) {//建立内部节点
+		x->child = B_vectorCreat(sizeof(SR_dataBTNode*));
+		x->childCode = B_vectorCreat(sizeof(SR_dataBTNode*));
+		x->goodsVector = NULL;
+		for (int i = 0; i < x->childNum; i++) {
+			SR_dataBTNode* x = (SR_dataBTNode*)malloc(sizeof(SR_dataBTNode));
+			B_getSrc(file, x, sizeof(SR_dataBTNode));
+		}
+	}
+	else {//建立外部节点
+		x->child = NULL;
+		x->childCode = NULL;
+		FILE* _file;
+		_file = fopen(x->fileName, "rb+");
+		x->goodsVector = B_vectorCreat(sizeof(C_Goods));
+		int flen = ftell(_file);
+		x->goodsVector->_capicity = flen / sizeof(C_Goods);
+		free(x->goodsVector->_elem);
+		x->goodsVector->_elem = (char*)malloc(flen);
+		B_getSrc(_file, x->goodsVector->_elem, flen);
+		x->goodsVector->_size = flen / sizeof(C_Goods);
+	}
 }
--- shop_assistant/src/c/main_test.c
+++ shop_assistant/src/c/main_test.c
@@ -1,3 +1,20 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include"B_list.h"
+#include<stdio.h>
+struct test {
+	int a;
+	int b;
+	int c;
+};
 int main() {
-
+	struct test t;
+	t.a = 1;
+	t.b = 2;
+	t.c = 3;
+	B_list* list = B_listCreat(sizeof(t));
+	B_listPushBack(list,&t);
+	struct test* cpy = B_listGetFirst(list);
+	printf("%d\n", cpy->b);
+	B_listRemoveFirst(list);
+	printf("%d", cpy->c);
 }
