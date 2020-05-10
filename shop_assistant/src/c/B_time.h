65b5487781c84786700b904043c3e3eb7acd9a83
Ainz <59441476+461720777@users.noreply.github.com>
2020-05-10 16:03:16 +0800

    Update B_time.h

--- shop_assistant/src/c/B_time.h
+++ shop_assistant/src/c/B_time.h
@@ -1,17 +1,15 @@
-/*
- * @Description: 
- * @Version: 2.0
- * @Autor: Charol
- * @Date: 2020-05-07 22:40:28
- * @LastEditors: 
- * @LastEditTime: 2020-05-07 22:40:48
- */
 //
-// Created by Charon on 2020/4/25.6666
-//6666
-
+// Created by Charon on 2020/4/25
+//
+#define _CRT_SECURE_NO_WARNINGS
 #ifndef B_TIME_B_TIME_H
 #define B_TIME_B_TIME_H
+#include <stdio.h>
+#include <time.h>
+#include <math.h>
+#define isPrime(year) ((year%4==0&&year%100!=0)||(year%400==0))
+
+
 typedef struct {
     char B_Day[4];
     char B_Year[4];
@@ -36,6 +34,8 @@ Date_C B_Time_C(void) ;//返回字符串型时间的函数
 Date B_Time_I(void) ;//返回整型时间的函数
 int B_DateIsSmall(Date x, Date y) ;//比较两个时间，x<y?1:0 ;
 int B_DayPassed(Date pre, Date lat);//计算两个日期间的天数，如有数据错误，返回-1；
-
+///////////////////
+void B_DateSet(Date* date, int y, int m, int d);//设定日期，个人新增
+Date B_DayLater(Date* date, int day);//几天后的日期
 
 #endif //B_TIME_B_TIME_H
